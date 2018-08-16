26f546f13dea90340a9afdaef360ebc6969fad15
dattatreya97 <dattu4621@gmail.com>
2018-08-16 23:15:51 +0530

    Added username change notification

--- index.js
+++ index.js
@@ -17,10 +17,19 @@ const io = require('socket.io')(server);
 
 io.on('connection', function(socket){
     
+    //this means a new connection! Add an event to notify 
+    socket.on('new_connection',function(data){
+        io.sockets.emit('new_connection');
+    })
+
+
     socket.username = "Guest";
     socket.on('change_username',function(data){
-        
         socket.username = data.username;
+    });
+
+    socket.on('update_username',function(data){
+        socket.broadcast.emit('update_username',{data_old:data.data_old,data_new:data.data_new});
     });
 
     socket.on('new_message',function(data){
@@ -31,5 +40,10 @@ io.on('connection', function(socket){
     });
     socket.on('typing',function(data){
         socket.broadcast.emit('typing',{username:socket.username});
-    })
+    });
+
+
+    socket.on('new_file_message',function(data){
+        socket.broadcast.emit('new_file_message',{file_message:data.file_message});
+    });
 });
--- public/chat.js
+++ public/chat.js
@@ -3,23 +3,26 @@ $(function(){
     var old_username="";
     var socket = io.connect("localhost:3000");
     var message = $("#message");
+    var file_message  =$("#file_message");
     var username = $("#username");
     var event_in_chat  = $("#event_in_chat"); 
     var send_username = $("#send_username");
     var send_message = $("#send_message");
     var chatroom = $("#chatroom");
+    var colors = ["red","blue","black","darkgreen"];
+    var min=0;
+    var max=3; 
 
     //Change username
     send_username.click(function(){             
         socket.emit('change_username',{username:username.val()});
         var result = "<p>";
-        if(old_username){
-            result = result + old_username;
-        }else{
-            result = result + "Guest";
-        }
-        result = result +" changed to "+username.val() +"</p>";
-        event_in_chat.append(result);
+        
+        //send a get request to server, server will emit, event is update_username
+        socket.emit('update_username',{
+            data_old:(old_username?old_username:"Guest"),
+            data_new:username.val()
+        });
         old_username = username.val();
     });
 
@@ -27,21 +30,44 @@ $(function(){
         socket.emit('typing');
     });
 
+    socket.on('update_username',function(data){
+        result = data.data_old +" changed to "+data.data_new +"</p>";
+        chatroom.append(result);
+    });
+
+
+
     socket.on('typing',function(data){
         var result = "<p><i>" + data.username  + " is typing something ...</i></p>";
         event_in_chat.html(result);
     });
     
-    send_message.click(function(){        
-        socket.emit('new_message',{message:message.val()});
-        message.val() = "";
+    send_message.click(function(){     
+        if(message.val()){   
+            socket.emit('new_message',{message:message.val()});
+            message.html(' ');
+        }
+        //trying file upload
+        if(file_message.files.length!=0){
+            socket.emit('new_file_message',{file_message:file_message.file()});
+        }
     });
 
     //When you receive a message, just display it
     socket.on('new_message',function(data){
-        var result = "<p><b>"+data.username+"</b> : "+data.message +"</p>";
+        var random =Math.floor(Math.random() * (+max - +min)) + +min;
+        var result = '<p><b style = "color: '+colors[random]+'">'+data.username+'</b> : '+data.message +'</p>';
         chatroom.append(result);
     });
 
+
+    socket.on('new_connection',function(data){
+        var result = '<p><b>new connection joined</b></p>';
+        chatroom.append(result);
+    });
+
+    socket.on('new_file_message',function(data){
+        
+    });
     
 });
